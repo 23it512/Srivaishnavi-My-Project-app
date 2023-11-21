746beeb49ea1bc6c7ee3e024490bfb5f09e6c36b
*Sri Vaishnavi*** <150108217+23it512@users.noreply.github.com>
2023-11-21 18:55:23 +0530

    Add files via upload

---
+++ Project/project/src/App.js
@@ -0,0 +1,30 @@
+// import logo from './logo.svg';
+import './App.css';
+import Navbar from './Project/Navbar';
+import Main from './Project/Main';
+import { Routes,Route } from 'react-router-dom';
+import { Login } from '@mui/icons-material';
+import SignUp from './Project/SignUp';
+import Footer from './Project/Footer';
+
+
+function App() {
+  return (
+      <>
+      <Navbar></Navbar>
+      <div className='container'>
+      <Routes>
+        <Route path="" element={<Main/>}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/sign" element={<SignUp/>}></Route>
+        <Route path="/main" element={<Main/>}></Route>
+      </Routes>
+      </div>
+      <Footer></Footer>
+      </>
+    
+  );
+}
+
+export default App;
+
